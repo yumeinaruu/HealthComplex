f3e420b7cf1a57d97a894a77cfb268dd0d611662
yumeinaruu <stanislav.lisovoj@krainet.by>
2024-11-28 21:15:17 +0300

    initial commit

---
+++ src/main/java/com/project/healthcomplex/HealthComplexApplication.java
@@ -0,0 +1,13 @@
+package com.project.healthcomplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthComplexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthComplexApplication.class, args);
+    }
+
+}
