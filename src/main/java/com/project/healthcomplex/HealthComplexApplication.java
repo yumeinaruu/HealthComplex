0c9f93004823bd7459574bb2f72ada674c858217
yumeinaruu <stanislav.lisovoj@krainet.by>
2024-11-28 22:33:40 +0300

    fix

--- src/main/java/com/project/healthcomplex/HealthComplexApplication.java
+++ src/main/java/com/project/healthcomplex/HealthComplexApplication.java
@@ -1,8 +1,18 @@
 package com.project.healthcomplex;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(
+        title = "Health complex",
+        description = "Health complex",
+        contact = @Contact(name = "Студент",
+                url = "https://github.com/",
+                email = "email")
+))
 @SpringBootApplication
 public class HealthComplexApplication {
 
